0a1c08a93b3017eba6df3ea1b8e0786da02482dc
David Lash <dlash02@gmail.com>
2024-08-27 17:23:56 -0500

    First One Prep

---
+++ Week1Prep/FirstOne/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Jane";
+        System.out.println("Welcome to my world");
+        System.out.printf("\n Welcome %s\n", name);
+    }
+}
